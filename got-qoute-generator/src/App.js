28453729f8efc8738c776b720e5989c11a741ae4
TimoKaufmann <kaufmann.timo.95@gmail.com>
2025-08-22 11:44:33 +0200

    refactored global styles, implemented component style module.

--- got-qoute-generator/src/App.js
+++ got-qoute-generator/src/App.js
@@ -3,30 +3,31 @@ import { Routes, Route, Link} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
+
 function App() {
 
-    return(
-      <div className="App">
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Game of Thrones Zitat-Generator</h1>
+        <p>Ein Ort für Weisheit (und Sarkasmus) aus Westeros.</p>
+        <nav className='main-nav'>
+          <Link to="/" className='nav-link'>Home </Link>
+          <Link to="/about" className='nav-link'>Über uns</Link>
+        </nav>
+      </header>
 
-          <header className="App-header">
-              <h1>Game of Thrones Zitat-Generator</h1>
-              <nav className="main-nav">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/about" className="nav-link">Über Uns</Link>
-              </nav>
-          </header>
-          <main className="app-main-content">
-              <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/about" element={<AboutPage/>} />
-                <Route path="*" element={<h2 style={{color:"white"}}>404 - Seite nicht gefunden</h2>} />
-              </Routes>
-          </main>
+      <main className='app-main-content'>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<h2 style={{color: 'white'}}>404 - Seite nicht gefunden!</h2>}/>
+        </Routes>
+      </main>
 
-      </div>
-    )
+    </div>
+  )
 
 }
 
-
-export default App
+export default App;
